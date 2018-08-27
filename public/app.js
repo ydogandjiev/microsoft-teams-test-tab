858631a56b17d6f43b51852d2bf3683e3a181c91
Sachin Gupta <saching@microsoft.com>
2018-08-27 16:59:05 -0700

    adding upload file capability in test app

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
   var inputs = {};
 
   var container = document.createElement("div");
@@ -22,23 +22,23 @@
 
         var input;
         var valueGetter;
-        switch(config.inputs[i].type) {
+        switch (config.inputs[i].type) {
           case "string":
             input = document.createElement("input");
-            valueGetter = function() {
+            valueGetter = function () {
               return input.value;
             };
             break;
           case "boolean":
             input = document.createElement("input");
             input.type = "checkbox"
-            valueGetter = function() {
+            valueGetter = function () {
               return input.checked;
             };
             break;
           case "object":
             input = document.createElement("textarea");
-            valueGetter = function() {
+            valueGetter = function () {
               input.style.backgroundColor = "";
               try {
                 return JSON.parse(input.value);
@@ -72,7 +72,7 @@
 
     container.appendChild(element);
 
-    button.addEventListener("click", function() {
+    button.addEventListener("click", function () {
       var args = [];
       if (config.inputs) {
         for (var i = 0; i < config.inputs.length; i++) {
@@ -86,7 +86,7 @@
       }
 
       if (config.hasOutput) {
-        args.push(function(result) {
+        args.push(function (result) {
           if (typeof result !== "string") {
             result = JSON.stringify(result);
           }
@@ -101,7 +101,7 @@
 
   addModule({
     name: "initialize",
-    action: function() {
+    action: function () {
       microsoftTeams.initialize();
     }
   });
@@ -109,17 +109,18 @@
   addModule({
     name: "getContext",
     hasOutput: true,
-    action: function(output) {
+    action: function (output) {
       microsoftTeams.getContext(output);
     }
   });
 
   addModule({
     name: "navigateCrossDomain",
-    inputs: [
-      { type: "string", name: "url"},
-    ],
-    action: function(url) {
+    inputs: [{
+      type: "string",
+      name: "url"
+    }, ],
+    action: function (url) {
       microsoftTeams.navigateCrossDomain(url);
     }
   });
@@ -127,34 +128,36 @@
   addModule({
     name: "registerOnThemeChangeHandler",
     hasOutput: true,
-    action: function(output) {
+    action: function (output) {
       microsoftTeams.registerOnThemeChangeHandler(output);
     }
   });
 
   addModule({
     name: "shareDeepLink",
-    inputs: [
-      { type: "object", name: "deepLinkParameters"},
-    ],
-    action: function(deepLinkParameters) {
+    inputs: [{
+      type: "object",
+      name: "deepLinkParameters"
+    }, ],
+    action: function (deepLinkParameters) {
       microsoftTeams.shareDeepLink(deepLinkParameters);
     }
   });
 
   addModule({
     name: "authentication.authenticate",
-    inputs: [
-      { type: "string", name: "url"},
-    ],
+    inputs: [{
+      type: "string",
+      name: "url"
+    }, ],
     hasOutput: true,
-    action: function(url, output) {
+    action: function (url, output) {
       microsoftTeams.authentication.authenticate({
         url: url,
-        successCallback: function(result) {
+        successCallback: function (result) {
           output("Success:" + result);
         },
-        failureCallback: function(reason) {
+        failureCallback: function (reason) {
           output("Failure:" + reason);
         },
       });
@@ -163,20 +166,22 @@
 
   addModule({
     name: "authentication.notifyFailure",
-    inputs: [
-      { type: "string", name: "reason"},
-    ],
-    action: function(reason) {
+    inputs: [{
+      type: "string",
+      name: "reason"
+    }, ],
+    action: function (reason) {
       microsoftTeams.authentication.notifyFailure(reason);
     }
   });
 
   addModule({
     name: "authentication.notifySuccess",
-    inputs: [
-      { type: "string", name: "result"},
-    ],
-    action: function(result) {
+    inputs: [{
+      type: "string",
+      name: "result"
+    }, ],
+    action: function (result) {
       microsoftTeams.authentication.notifySuccess(result);
     }
   });
@@ -184,7 +189,7 @@
   addModule({
     name: "settings.getSettings",
     hasOutput: true,
-    action: function(output) {
+    action: function (output) {
       microsoftTeams.settings.getSettings(output);
     }
   });
@@ -192,8 +197,8 @@
   addModule({
     name: "settings.registerOnSaveHandler",
     hasOutput: true,
-    action: function(output) {
-      microsoftTeams.settings.registerOnSaveHandler(function(saveEvent) {
+    action: function (output) {
+      microsoftTeams.settings.registerOnSaveHandler(function (saveEvent) {
         window.saveEvent = saveEvent;
         output("SaveEvent recieved");
       });
@@ -202,68 +207,78 @@
 
   addModule({
     name: "settings.registerOnSaveHandler.notifyFailure",
-    inputs: [
-      { type: "string", name: "reason" }
-    ],
-    action: function(reason) {
+    inputs: [{
+      type: "string",
+      name: "reason"
+    }],
+    action: function (reason) {
       window.saveEvent && window.saveEvent.notifyFailure(reason);
     }
   });
 
   addModule({
     name: "settings.registerOnSaveHandler.notifySuccess",
-    action: function() {
+    action: function () {
       window.saveEvent && window.saveEvent.notifySuccess();
     }
   });
 
   addModule({
     name: "settings.setSettings",
-    inputs: [
-      { type: "object", name: "settings" }
-    ],
-    action: function(settings) {
+    inputs: [{
+      type: "object",
+      name: "settings"
+    }],
+    action: function (settings) {
       microsoftTeams.settings.setSettings(settings);
     }
   });
 
   addModule({
     name: "settings.setValidityState",
-    inputs: [
-      { type: "boolean", name: "validityState" }
-    ],
-    action: function(validityState) {
+    inputs: [{
+      type: "boolean",
+      name: "validityState"
+    }],
+    action: function (validityState) {
       microsoftTeams.settings.setValidityState(validityState);
     }
   });
 
   addModule({
     name: "openFilePreview",
-    inputs: [
-      { type: "object", name: "filePreviewParameters" }
-    ],
-    action: function(filePreviewParameters) {
+    inputs: [{
+      type: "object",
+      name: "filePreviewParameters"
+    }],
+    action: function (filePreviewParameters) {
       microsoftTeams.openFilePreview(filePreviewParameters);
     }
   });
 
   addModule({
     name: "task.submitTask",
-    inputs: [
-      { type: "string", name: "result" },
-      { type: "string", name: "appId" },
+    inputs: [{
+        type: "string",
+        name: "result"
+      },
+      {
+        type: "string",
+        name: "appId"
+      },
     ],
-    action: function(result, appId) {
+    action: function (result, appId) {
       microsoftTeams.tasks.submitTask(result, appId);
     }
   });
 
   addModule({
     name: "tasks.startTask",
-    inputs: [
-      { type: "object", name: "taskInfo" },
-    ],
-    action: function(taskInfo) {
+    inputs: [{
+      type: "object",
+      name: "taskInfo"
+    }, ],
+    action: function (taskInfo) {
       microsoftTeams.tasks.startTask(taskInfo);
     }
   });
@@ -294,4 +309,69 @@
   // Give the DOM a chance to update from the appendChild above
   setTimeout(restoreState, 0);
   window.addEventListener("beforeunload", saveState);
+
+  var input = document.querySelector('#image_uploads');
+  var preview = document.querySelector('.fileUploadPreview');
+
+  input.style.opacity = 0;
+  input.addEventListener('change', updateImageDisplay);
+
+  function updateImageDisplay() {
+    while (preview.firstChild) {
+      preview.removeChild(preview.firstChild);
+    }
+
+    var curFiles = input.files;
+    if (curFiles.length === 0) {
+      var para = document.createElement('p');
+      para.textContent = 'No files currently selected for upload';
+      preview.appendChild(para);
+    } else {
+      var list = document.createElement('ol');
+      preview.appendChild(list);
+      for (var i = 0; i < curFiles.length; i++) {
+        var listItem = document.createElement('li');
+        var para = document.createElement('p');
+        if (validFileType(curFiles[i])) {
+          para.textContent = 'File name ' + curFiles[i].name + ', file size ' + returnFileSize(curFiles[i].size) + '.';
+          var image = document.createElement('img');
+          image.src = window.URL.createObjectURL(curFiles[i]);
+
+          listItem.appendChild(image);
+          listItem.appendChild(para);
+
+        } else {
+          para.textContent = 'File name ' + curFiles[i].name + ': Not a valid file type. Update your selection.';
+          listItem.appendChild(para);
+        }
+
+        list.appendChild(listItem);
+      }
+    }
+  }
+  var fileTypes = [
+    'image/jpeg',
+    'image/pjpeg',
+    'image/png'
+  ]
+
+  function validFileType(file) {
+    for (var i = 0; i < fileTypes.length; i++) {
+      if (file.type === fileTypes[i]) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  function returnFileSize(number) {
+    if (number < 1024) {
+      return number + 'bytes';
+    } else if (number >= 1024 && number < 1048576) {
+      return (number / 1024).toFixed(1) + 'KB';
+    } else if (number >= 1048576) {
+      return (number / 1048576).toFixed(1) + 'MB';
+    }
+  }
 })();
