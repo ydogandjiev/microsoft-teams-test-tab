785845049323d269c6644753cd2190cd468c0b3c
Sachin Gupta <saching@microsoft.com>
2018-08-27 17:23:35 -0700

    fixing some js changes

--- public/app.js
+++ public/app.js
@@ -310,68 +310,72 @@
   setTimeout(restoreState, 0);
   window.addEventListener("beforeunload", saveState);
 
-  var input = document.querySelector('#image_uploads');
-  var preview = document.querySelector('.fileUploadPreview');
 
-  input.style.opacity = 0;
-  input.addEventListener('change', updateImageDisplay);
+  function onDocumentLoad() {
 
-  function updateImageDisplay() {
-    while (preview.firstChild) {
-      preview.removeChild(preview.firstChild);
-    }
+    var input = document.querySelector('#image_uploads');
+    var preview = document.querySelector('.fileUploadPreview');
 
-    var curFiles = input.files;
-    if (curFiles.length === 0) {
-      var para = document.createElement('p');
-      para.textContent = 'No files currently selected for upload';
-      preview.appendChild(para);
-    } else {
-      var list = document.createElement('ol');
-      preview.appendChild(list);
-      for (var i = 0; i < curFiles.length; i++) {
-        var listItem = document.createElement('li');
+    input.style.opacity = 0;
+    input.addEventListener('change', updateImageDisplay);
+
+    function updateImageDisplay() {
+      while (preview.firstChild) {
+        preview.removeChild(preview.firstChild);
+      }
+
+      var curFiles = input.files;
+      if (curFiles.length === 0) {
         var para = document.createElement('p');
-        if (validFileType(curFiles[i])) {
-          para.textContent = 'File name ' + curFiles[i].name + ', file size ' + returnFileSize(curFiles[i].size) + '.';
-          var image = document.createElement('img');
-          image.src = window.URL.createObjectURL(curFiles[i]);
+        para.textContent = 'No files currently selected for upload';
+        preview.appendChild(para);
+      } else {
+        var list = document.createElement('ol');
+        preview.appendChild(list);
+        for (var i = 0; i < curFiles.length; i++) {
+          var listItem = document.createElement('li');
+          var para = document.createElement('p');
+          if (validFileType(curFiles[i])) {
+            para.textContent = 'File name ' + curFiles[i].name + ', file size ' + returnFileSize(curFiles[i].size) + '.';
+            var image = document.createElement('img');
+            image.src = window.URL.createObjectURL(curFiles[i]);
 
-          listItem.appendChild(image);
-          listItem.appendChild(para);
+            listItem.appendChild(image);
+            listItem.appendChild(para);
 
-        } else {
-          para.textContent = 'File name ' + curFiles[i].name + ': Not a valid file type. Update your selection.';
-          listItem.appendChild(para);
+          } else {
+            para.textContent = 'File name ' + curFiles[i].name + ': Not a valid file type. Update your selection.';
+            listItem.appendChild(para);
+          }
+
+          list.appendChild(listItem);
         }
-
-        list.appendChild(listItem);
       }
     }
-  }
-  var fileTypes = [
-    'image/jpeg',
-    'image/pjpeg',
-    'image/png'
-  ]
+    var fileTypes = [
+      'image/jpeg',
+      'image/pjpeg',
+      'image/png'
+    ]
 
-  function validFileType(file) {
-    for (var i = 0; i < fileTypes.length; i++) {
-      if (file.type === fileTypes[i]) {
-        return true;
+    function validFileType(file) {
+      for (var i = 0; i < fileTypes.length; i++) {
+        if (file.type === fileTypes[i]) {
+          return true;
+        }
       }
+
+      return false;
     }
 
-    return false;
-  }
-
-  function returnFileSize(number) {
-    if (number < 1024) {
-      return number + 'bytes';
-    } else if (number >= 1024 && number < 1048576) {
-      return (number / 1024).toFixed(1) + 'KB';
-    } else if (number >= 1048576) {
-      return (number / 1048576).toFixed(1) + 'MB';
+    function returnFileSize(number) {
+      if (number < 1024) {
+        return number + 'bytes';
+      } else if (number >= 1024 && number < 1048576) {
+        return (number / 1024).toFixed(1) + 'KB';
+      } else if (number >= 1048576) {
+        return (number / 1048576).toFixed(1) + 'MB';
+      }
     }
   }
 })();
