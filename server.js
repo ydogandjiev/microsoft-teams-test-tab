0cdd8887a7da4c1395a3f14be0123bd477e82687
Yuri Dogandjiev <yuri.dogandjiev@live.com>
2019-12-10 17:58:42 -0800

    Update package and block caching

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express');
 var serveStatic = require('serve-static');
 
 var app = express();
-app.use(serveStatic(__dirname + '/public'));
+app.use(serveStatic(__dirname + '/public', {
+  setHeaders: (res, path) => {
+    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  }
+}));
 
 var port = process.env.port || 3000;
 app.listen(port, function () {
