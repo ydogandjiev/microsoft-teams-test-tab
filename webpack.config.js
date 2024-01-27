537875df0dbf7bd227f7efd9d96cb46d06def526
Yuri Dogandjiev <yuri.dogandjiev@live.com>
2024-01-27 13:10:34 -0800

    Move all pages to hashed script file

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,26 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Teams Test Tab'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Auth Start Page',
+      filename: 'auth_start.html',
+      template: 'src/auth_start.ejs'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Auth End Page',
+      filename: 'auth_end.html',
+      template: 'src/auth_end.ejs'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Page 1',
+      filename: 'page1.html',
+      template: 'src/page1.ejs'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Page 2',
+      filename: 'page2.html',
+      template: 'src/page2.ejs'
     })
   ],
   module: {
