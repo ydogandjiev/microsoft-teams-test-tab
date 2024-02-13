b2b6978bd2cd1d59892c0495d272818c45687bf6
Yuri Dogandjiev <yuri.dogandjiev@live.com>
2024-02-12 19:23:08 -0800

    Only include SDK once

--- webpack.config.js
+++ webpack.config.js
@@ -5,27 +5,32 @@ module.exports = {
   entry: './src/index.ts',
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Teams Test Tab'
+      title: 'Teams Test Tab',
+      inject: false
     }),
     new HtmlWebpackPlugin({
       title: 'Auth Start Page',
       filename: 'auth_start.html',
-      template: 'src/auth_start.ejs'
+      template: 'src/auth_start.ejs',
+      inject: false
     }),
     new HtmlWebpackPlugin({
       title: 'Auth End Page',
       filename: 'auth_end.html',
-      template: 'src/auth_end.ejs'
+      template: 'src/auth_end.ejs',
+      inject: false
     }),
     new HtmlWebpackPlugin({
       title: 'Page 1',
       filename: 'page1.html',
-      template: 'src/page1.ejs'
+      template: 'src/page1.ejs',
+      inject: false
     }),
     new HtmlWebpackPlugin({
       title: 'Page 2',
       filename: 'page2.html',
-      template: 'src/page2.ejs'
+      template: 'src/page2.ejs',
+      inject: false
     })
   ],
   module: {
